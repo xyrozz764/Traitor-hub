ddd11ced5a3b9b9f4d56d25aaa21b24f2d54d010
xyrozz764 <justmakey@gmail.com>
2025-04-27 12:38:49 +0800

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,12 +1,8 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyrozz764/Traitor-hub/refs/heads/main/Init.lua..."))()
-‎local Window = Library.CreateLib("Tutorial", "BloodTheme")
-‎
-‎local Tab = Window:NewTab("click")
-‎local Section = Tab:NewSection("click")
-‎
-‎Section:NewLabel("click")
-‎
-‎Section:NewButton("click", "justclick", function()
-‎  loadstring(game:HttpGet("https://raw.githubusercontent.com/xyrozz764/Traitor-hub/refs/heads/main/Init.lua..."))()  
-‎end)
-‎
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyrozz764/Traitor-hub/main/Init.lua"))()
+local Window = Library:CreateLib("Tutorial", "BloodTheme")
+local Tab = Window:NewTab("Click")
+local Section = Tab:NewSection("Click")
+Section:NewLabel("Click")
+Section:NewButton("Click", "Just click", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xyrozz764/Traitor-hub/main/Init.lua"))()
+end)
