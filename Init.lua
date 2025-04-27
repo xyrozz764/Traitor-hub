b65da4c2bb5272460baec641799b039fadaa169d
xyrozz764 <justmakey@gmail.com>
2025-04-27 12:32:44 +0800

    Create Init.lua

---
+++ Init.lua
@@ -0,0 +1,12 @@
+local Library = loadstring(game:HttpGet("https://raw.githubuser..."))()
+‎local Window = Library.CreateLib("Tutorial", "BloodTheme")
+‎
+‎local Tab = Window:NewTab("click")
+‎local Section = Tab:NewSection("click")
+‎
+‎Section:NewLabel("click")
+‎
+‎Section:NewButton("click", "justclick", function()
+‎    
+‎end)
+‎
